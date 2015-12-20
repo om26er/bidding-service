5aca3e0b2d3c19badd7ac374493c0dda39ba91ed
Omer Akram <om26er@gmail.com>
2015-12-21 03:56:55 +0500

    More fix

--- login/admin.py
+++ login/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from login.models import CustomUser
+from login.models import customuser
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     can_delete = False
     verbose_name_plural = 'userprofile'
 
-admin.site.register(CustomUser, UserProfileAdmin)
+admin.site.register(customuser, UserProfileAdmin)
