42c95433ddba3b688a0f6873d117fd7205a12a5b
Omer Akram <om26er@gmail.com>
2015-12-21 04:28:32 +0500

    Bring configuration inline with heroku guides

--- accounts/settings.py
+++ accounts/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'accounts.wsgi:application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES = {'default': dj_database_url.config()}
-DATABASES['default']['CONN_MAX_AGE'] = 500
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
