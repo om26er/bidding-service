bf379fb3bf3ed43412cb825be3eeac7460fe7f99
Omer Akram <om26er@gmail.com>
2015-12-22 00:05:19 +0500

    Update static files storage

--- accounts/settings.py
+++ accounts/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.expanduser('~/Pictures')
 
