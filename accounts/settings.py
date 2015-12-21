593d076de48460a55b4356432ff2db5c46128b32
Omer Akram <om26er@gmail.com>
2015-12-21 23:09:00 +0500

    Remove hard-coded credentials

--- accounts/settings.py
+++ accounts/settings.py
@@ -94,10 +94,8 @@ WSGI_APPLICATION = 'accounts.wsgi.application'
 #     }
 # }
 
-DATABASES = {'default': dj_database_url.config(
-    default='postgres://dkvtxytitilzva:Rlyos5YiZEhW2UDSpf6G3lX7wC@ec2-75-101-'
-            '143-150.compute-1.amazonaws.com:5432/dcqr8q0stmpvjp')}
-# DATABASES['default']['CONN_MAX_AGE'] = 500
+DATABASES = {'default': dj_database_url.config()}
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
