d2c55b003af38b767a83ce572879e2e82a73a063
Omer Akram <om26er@gmail.com>
2015-12-31 15:53:31 +0500

    Upload unique name photos

--- login/models.py
+++ login/models.py
@@ -1,21 +1,25 @@
+import uuid
+import os
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from accounts.settings import AUTH_USER_MODEL
 
-from login.helpers import get_file_path
-
-AD_IMAGES = 'attachments'
-# CATEGORY_IMAGES = 'categories'
-
 CHOICES = [(i, i) for i in range(1, 6)]
+
+
+def get_image_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(uuid.uuid4(), ext)
+    return os.path.join('images', filename)
 
 
 class CustomUser(AbstractUser):
     address = models.CharField(max_length=500)
     phone_number = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='avatars')
+    photo = models.ImageField(upload_to=get_image_file_path)
     interests = models.CharField(max_length=1000, blank=True)
     push_key = models.CharField(max_length=1000, blank=True)
 
@@ -30,14 +34,14 @@ class ProductAd(models.Model):
     description = models.CharField(max_length=4000, blank=False)
     category = models.CharField(max_length=100, blank=False)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
-    photo1 = models.ImageField(upload_to=AD_IMAGES)
-    photo2 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo3 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo4 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo5 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo6 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo7 = models.ImageField(upload_to=AD_IMAGES, blank=True)
-    photo8 = models.ImageField(upload_to=AD_IMAGES, blank=True)
+    photo1 = models.ImageField(upload_to=get_image_file_path)
+    photo2 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo3 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo4 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo5 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo6 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo7 = models.ImageField(upload_to=get_image_file_path, blank=True)
+    photo8 = models.ImageField(upload_to=get_image_file_path, blank=True)
 
 
 class Comments(models.Model):
@@ -57,5 +61,5 @@ class Comments(models.Model):
 class AdCategories(models.Model):
 
     name = models.CharField(max_length=70, blank=False, unique=True)
-    photo = models.ImageField(upload_to=get_file_path, blank=False)
+    photo = models.ImageField(upload_to=get_image_file_path, blank=False)
     id = models.AutoField(primary_key=True)
