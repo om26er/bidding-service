946ae40148e3ad2029af16d0dd3890992fe16854
Omer Akram <om26er@gmail.com>
2016-04-13 16:15:17 +0500

    Also send email address of ad owner

--- login/serializers.py
+++ login/serializers.py
@@ -63,6 +63,7 @@ class AdSerializer(serializers.ModelSerializer):
     currency = serializers.CharField(required=True)
     delivery_time = serializers.CharField(required=True)
     owner = serializers.CharField(read_only=True)
+    owner_email = serializers.CharField(read_only=True)
     sold = serializers.BooleanField(required=False)
     sold_to = serializers.CharField(required=False)
 
@@ -79,7 +80,7 @@ class AdSerializer(serializers.ModelSerializer):
         fields = ('id', 'created', 'title', 'description', 'category', 'price',
                   'photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'photo6',
                   'photo7', 'photo8', 'currency', 'bids', 'delivery_time',
-                  'owner', 'sold', 'sold_to')
+                  'owner', 'sold', 'sold_to', 'owner_email')
 
 
 class AdBidSerializer(serializers.ModelSerializer):
